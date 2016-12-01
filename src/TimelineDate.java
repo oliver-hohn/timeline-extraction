916f250f720f23f0cff9f85bb0c8c791ec24dd3b
Oliver Höhn <oliver.hohn@kcl.ac.uk>
2016-12-01 13:48:22 +0000

    TimelineDate.java: -will be used to parse the String dates given by StanfordCoreNLP to actual dates to then use in the timeline. -wrote pseudo-code for parse() (in comments)

---
+++ src/TimelineDate.java
@@ -0,0 +1,34 @@
+/**
+ * Represents the dates for a timeline event.
+ */
+public class TimelineDate {
+    private String year;
+    private String month;
+    private String day;
+
+    public TimelineDate(){
+        year = "0001";
+        month = "01";
+        day = "01";
+    }
+
+    /**
+     *
+     * @param date a date provided by the StanfordCoreNLP library: it is a normalized entity
+     */
+    public void parse(String date){
+        //if it starts with YYYY
+            //split to check for range
+                //split into year, month, and day
+                //check what format it follows: YYYY, YYYY-MM, YYYY-dd-dd ,YYYY-Wdd, YYYY-Season ,YYYX, YYXX, YXXX
+                    //for YYYY just set it for the year
+                    //for YYYY-MM split it and set it for year and month
+                    //for YYYY-mm-dd split and set it for year, month and day
+                    //for YYYY-Wdd split it, set the date, and calculate the MM from Wdd
+                    //for YYYY-Season split it, produce two dates (its a range), set year for both and put start month and day for that season, and for other date put end month and day for that season
+                    //for YYYX make two dates, one for YYY0 and YYY9
+                    //for YYXX make two dates, one for YY00 and YY99
+                    //for YXXX make two dates, one for Y000 and Y999
+        //add dates to list of dates
+    }
+}
