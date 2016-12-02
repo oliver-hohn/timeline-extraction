069070d2df3e3aa72c5e01eff2dd0df8cb132b89
Oliver Höhn <oliver.hohn@kcl.ac.uk>
2016-12-02 20:03:48 +0000

    Engine.java: -calling TimelineDate.parse() to see how our date extraction system is working with the normalized dates provided by the StanfordCoreNLP
    TestClass.java:
    -added an extra sentence to see how the StanfordCoreNLP gives normalized dates for when an event is repeated every year on a certain month.
    
    TimelineDate.java:
    -added the Patterns for the different formats found up to now for the Normalized Dates provided by the StanfordCoreNLP. Identifies just years, years and months (even with the years that are unspecified), years,months and days, years and week number, years and season.
    -added comments for format when years are unknown but month is: where the text is of the type XXXX-MM INTERSECT P1Y.

--- src/TimelineDate.java
+++ src/TimelineDate.java
@@ -7,9 +7,14 @@ public class TimelineDate {
     private String year;
     private String month;
     private String day;
-
+                                                                    //wont pick up for dates like: every year in january, which has format XXXX-01
     private final static Pattern yearPattern = Pattern.compile("(\\d{4}.*$)|(\\d{3}X.*$)|(\\d{2}XX.*$)|(\\dXXX.*$)");//pattern to check the start of the sentence matching year
-
+    private final static Pattern onlyYearPattern = Pattern.compile("(\\d{4})|(\\d{3}X)|(\\d{2}XX)|(\\dXXX)");
+    private final static Pattern yearMonthPattern = Pattern.compile("\\d{4}\\-\\d{2}");
+    private final static Pattern yearMonthDayPattern = Pattern.compile("\\d{4}\\-\\d{2}\\-\\d{2}");
+    private final static Pattern yearWeekNumberPattern = Pattern.compile("\\d{4}\\-W\\d{2}");
+    private final static Pattern yearSeasonPattern = Pattern.compile("\\d{4}\\-[A-Z]{2}");
+    private final static Pattern yearPatternWithX = Pattern.compile("(\\d{3}X)|(\\d{2}XX)|(\\dXXX)");
     public TimelineDate(){
         year = "0001";
         month = "01";
@@ -22,9 +27,59 @@ public class TimelineDate {
      */
     public void parse(String date){
         //if it starts with YYYY
+        System.out.println("Passing date: "+date);
         if(yearPattern.matcher(date).matches()) {
-            System.out.println("Date is legal: "+date);
+            //System.out.println("Date is legal: "+date);
             //split to check for range
+            //System.out.println(date.split("/").length);
+            String[] splitDate = date.split("/");
+            for(int i=0; i<splitDate.length; i++){
+                //split into year month and day
+                if(onlyYearPattern.matcher(splitDate[i]).matches()){//checking just for year dates
+                    System.out.println("Date: "+splitDate[i]+ " is just a year YYYY");
+                    if(yearPatternWithX.matcher(splitDate[i]).matches()) {
+                        //need to check X's (check if it has any), for first, replace every X with 0, for last replace every X with 9
+                        String startYear = splitDate[i].replaceAll("X", "0");
+                        String endYear = splitDate[i].replaceAll("X", "9");
+
+                        System.out.println("Our years for: "+splitDate[i]+" is: "+startYear+" - "+endYear);
+                    }
+                    //break into year and set it
+                }else if(yearMonthPattern.matcher(splitDate[i]).matches()){//checking just for year-month dates
+                    System.out.println("Date: "+splitDate[i]+" has the format YYYY-MM");
+                    //break into year and month and set it
+                    String[] splitString = splitDate[i].split("-");//as we are in if, the string has format YYYY-MM can split it
+                    String year = splitString[0];
+                    String month = splitString[1];
+
+                    System.out.println("For: "+splitDate[i]+" we have as year: "+year+" and month: "+month);
+                }else if(yearMonthDayPattern.matcher(splitDate[i]).matches()){//checking for year-month-date
+                    System.out.println("Date: "+splitDate[i]+" has the format YYYY-MM-DD");
+                    //break into year, month and day, or just set it to date directly
+                    String[] splitString = splitDate[i].split("-");//because of above matching we know we have a string of the format YYYY-MM-DD
+                    String year = splitString[0];
+                    String month = splitString[1];
+                    String day = splitString[2];
+
+                    System.out.println("For: "+splitDate[i]+" we have as year: "+year+" , month: "+month+" and day: "+day);
+                }else if(yearWeekNumberPattern.matcher(splitDate[i]).matches()){//checking for year-weeknumber
+                    System.out.println("Date: "+splitDate[i]+" has the format YYYY-Wdd");
+                    //break into year and weeknumber, then calculate month from week number
+                    String[] splitString = splitDate[i].split("-");
+                    String year = splitString[0];
+                    String weekNo = splitString[1].substring(1);//need to remove the W to just get the week number, gives you substring from position 1
+
+                    System.out.println("For: "+splitDate[i]+" we have as year: "+year+" and week number: "+weekNo);
+                }else if(yearSeasonPattern.matcher(splitDate[i]).matches()){//checking for year-season
+                    System.out.println("Date: "+splitDate[i]+" has the format YYYY-Season");
+                    //break into year and season, then create two date objects, with same year, one with start season month and day and other with end season month and day
+                    String[] splitString = splitDate[i].split("-");
+                    String year = splitString[0];
+                    String season = splitString[1];
+
+                    System.out.println("For: "+splitDate[i]+" we have as year: "+year+" and season: "+season);
+                }
+            }
             //split into year, month, and day
             //check what format it follows: YYYY, YYYY-MM, YYYY-dd-dd ,YYYY-Wdd, YYYY-Season ,YYYX, YYXX, YXXX
             //for YYYY just set it for the year
@@ -35,6 +90,9 @@ public class TimelineDate {
             //for YYYX make two dates, one for YYY0 and YYY9
             //for YYXX make two dates, one for YY00 and YY99
             //for YXXX make two dates, one for Y000 and Y999
+            //for XXXX-MM range from 0000-MM to 9999-MM
+            //every year in month:  XXXX-MM INTERSECT P1Y
+
             //add dates to list of dates
         }
     }
