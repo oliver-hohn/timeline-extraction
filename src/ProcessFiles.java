c1144b1198928ab87523e6f80d79065b69c7a931
Oliver <oliver.hohn@kcl.ac.uk>
2016-12-19 00:27:10 +0100

    Added the stem for the ProcessFiles class. Added partial comments for what to do for each file type

---
+++ src/ProcessFiles.java
@@ -0,0 +1,9 @@
+/**
+ * Handles the parsing of files, and the multi-threading of the Engine.
+ */
+public class ProcessFiles {
+    //different input file types:
+    /*
+        .txt: readAllLines(Path path)
+     */
+}
