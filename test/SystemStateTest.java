c5e322c55895e8365b02cc8e1d4a3a8342f2b836
Oliver <oliver.hohn@kcl.ac.uk>
2016-12-23 22:46:32 +0100

    added empty test classes, and TODO messages to know what to work on

---
+++ test/SystemStateTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Oliver on 23/12/2016.
+ */
+//TODO: change SystemStates and check the output matches
+public class SystemStateTest {
+}
---
+++ test/TimelineDateTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Oliver on 23/12/2016.
+ */
+//TODO: pass in dates, and compare it to expected produced Dates
+public class TimelineDateTest {
+}
