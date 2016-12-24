28fb7162bdf29bc648b58867fca64400b9199f35
Oliver <oliver.hohn@kcl.ac.uk>
2016-12-24 11:41:36 +0100

    SystemStateTest.java: -tests the changes of the Systems state, by processing Files. Checks the state before we do this, then while it is processing a File (in a separate Thread), and finally when it received the Results. -added documentation -formatted the code.

--- test/SystemStateTest.java
+++ test/SystemStateTest.java
@@ -1,6 +1,49 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
 /**
- * Created by Oliver on 23/12/2016.
+ * Test class to test the change of System States as files are being Processed. Checks for STARTED, PROCESSING, and PROCESSED
+ * System States, as these are the three main different states.
  */
-//TODO: change SystemStates and check the output matches
 public class SystemStateTest {
+    private CallbackResults callbackResults = new CallbackResults() {
+        @Override
+        public void gotResults(ArrayList<Result> results) {
+            //we just finished processing a File, so our SystemState should be PROCESSED (FINISHED set after it passes the Results).
+            System.out.println("Checking if SystemState is PROCESSED");
+            Assert.assertEquals(BackEndSystem.getInstance().getSystemState(), SystemState.PROCESSED);
+        }
+    };
+
+    /**
+     * Checks the initial state, then when processing, and then when it should be finished.
+     *
+     * @throws InterruptedException as we make the Test thread wait for ProcessFile to finish processing the Files.
+     */
+    @Test
+    public void testSystemState() throws InterruptedException {
+        BackEndSystem.getInstance().setSystemState(SystemState.STARTED);//will start the system if it hasnt started before, if it has reset the SystemState
+        System.out.println("Checking if SystemState is STARTED");
+        Assert.assertEquals(BackEndSystem.getInstance().getSystemState(), SystemState.STARTED);//check the system is started
+
+        ProcessFiles processFiles = new ProcessFiles();
+
+        File testFile1 = new File("D:" + File.separator + "FYP" + File.separator + "testfile.txt");
+        File testFile2 = new File("D:" + File.separator + "FYP" + File.separator + "testfile2.txt");
+        File testFile3 = new File("D:" + File.separator + "FYP" + File.separator + "testfile3.txt");
+
+        ArrayList<File> files = new ArrayList<>();
+        files.add(testFile1);
+        files.add(testFile2);
+        files.add(testFile3);
+
+        processFiles.processFiles(files, callbackResults);
+        //its processing Files, so its status should be: PROCESSING
+        System.out.println("Checking if SystemState is PROCESSING");
+        Assert.assertEquals(BackEndSystem.getInstance().getSystemState(), SystemState.PROCESSING);
+        Thread.sleep(5000);//wait for the ProcessFile to be completed
+    }
 }
