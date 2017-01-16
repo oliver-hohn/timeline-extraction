a726336182c392f92a09e40403e21d81288a9c45
Oliver <oliver.hohn@kcl.ac.uk>
2017-01-16 15:48:16 +0000

    timelineRowEven.fxml and timelineRowOddfxml: -added the corresponding fx:id's for the buttons.
    TimelineRowController.java:
    -annotated buttons, and added event handlers for each (need to add implementation).

--- src/frontend/TimelineRowController.java
+++ src/frontend/TimelineRowController.java
@@ -1,9 +1,12 @@
 package frontend;
 
 import backend.process.Result;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -26,7 +29,12 @@ public class TimelineRowController {
     private Label eventLabel;
     @FXML
     private BorderPane borderPane;
+    @FXML
+    private Button editButton;
+    @FXML
+    private Button viewButton;
     private int position;
+    private Result result;
 
     public TimelineRowController(int position){
         this.position = position;
@@ -43,14 +51,32 @@ public class TimelineRowController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println("Edit: "+editButton);
+        System.out.println("View: "+viewButton);
     }
 
     public void setData(Result result){
+        this.result = result;
         eventNumberLabel.setText("Event #"+(position+1));
         dateLabel.setText("Date: "+result.getTimelineDate().toString());
         subjectsLabel.setText("Subjects: "+result.getSubjects().toString());
         eventLabel.setText("Event: "+result.getEvent());
         borderPane.setStyle("-fx-border-color: black; -fx-border-width: 4; -fx-border-style: solid inside;");
+        editButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Edit button for timeline event: "+result.getTimelineDate()+" has been pressed");
+                //TODO: edit button implementation (edit dialog screen)
+            }
+        });
+        viewButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Go to Document button for timeline event: "+result.getTimelineDate()+" has been pressed");
+                //TODO: go to document implementation (file reader with related sentence highlighted).
+            }
+        });
     }
 
     public Group getGroup(){
