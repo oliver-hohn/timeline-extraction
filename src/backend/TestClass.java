1bcf0d86b789e6e826cc61c4d9c53adf9b1de909
Oliver <oliver.hohn@kcl.ac.uk>
2017-01-22 19:19:59 +0000

    TestClass.java: -added library TODO

--- src/backend/TestClass.java
+++ src/backend/TestClass.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 /**
  * Created by Oliver on 29/10/2016.
  */
+//TODO: pass libraries to project directory
 public class TestClass {
 
     public static void main(String[] args) {
