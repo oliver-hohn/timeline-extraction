8f5a6128bb5de1552ccbffca5432cc274c9da110
Oliver <oliver.hohn@kcl.ac.uk>
2017-02-18 20:11:37 +0000

    TimelineRowObserver.java: -added documentation -removed completed TODO

--- src/main/java/frontend/observers/TimelineRowObserver.java
+++ src/main/java/frontend/observers/TimelineRowObserver.java
@@ -6,7 +6,6 @@ import backend.process.Result;
  * Implemented by the Observer of this TimelineRow (that is the holder of the ListView), so that the cells can communicate
  * with the overall listView.
  */
-//TODO: documentation
 public interface TimelineRowObserver {
     /**
      * Called by a TimelineRow to inform the Observer (holder of the ListView), that the data it is holding has been
@@ -17,6 +16,13 @@ public interface TimelineRowObserver {
      */
     void update(Result updatedResult, int position);
 
+    /**
+     * Called by a TimelineRow to inform the Observer (holder of the ListView), that the data it is holding has been
+     * updated.
+     *
+     * @param previous      the Result object that was previously in this Row.
+     * @param updatedResult the updated Result object of the TimelineRow.
+     */
     void update(Result previous, Result updatedResult);
 
     /**
@@ -27,6 +33,11 @@ public interface TimelineRowObserver {
      */
     void delete(int position);
 
-
+    /**
+     * Called by a TimelineRow to inform the Observer (holder of the ListView), that this event (the given Result) needs
+     * to be deleted from the list.
+     *
+     * @param result the given event (Result) to be deleted.
+     */
     void delete(Result result);
 }
